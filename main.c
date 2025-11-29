6aa04d6df3034026783eeb67c6b08c2d03eafa37
KunalChaudhary-hub <honouredlko@gmail.com>
2025-11-29 15:02:31 +0000

    Indentation

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 #include<string.h>
 #include<time.h>
 
-typedef struct{
+typedef struct{ // for storing appointment slot of every doc
     int slotNumber;
     char patientName[64];
 }Appointment;
 
-typedef struct{
+typedef struct{ // doctor details
     int id;
     char name[64];
     char field[64]; 
     int capacity;
-    Appointment *schedule;
+    Appointment *schedule; // dynamic list of appointment
     int bookedCount;
 }Doctor;
 
-Doctor *docList = NULL;
+Doctor *docList = NULL; // dymic list of doctors
 int docCount = 0;
-
+// add a doctor with given no. of slots
 void addDoctor(int id,const char *name,const char*field,int slots){
     docList = realloc(docList,sizeof(Doctor)*(docCount + 1));
     Doctor *d = &docList[docCount];
@@ -29,14 +29,14 @@ void addDoctor(int id,const char *name,const char*field,int slots){
     strcpy(d->field,field);
     d->capacity = slots;
     d->bookedCount = 0;
-    d->schedule = malloc(sizeof(Appointment)*slots);
+    d->schedule = malloc(sizeof(Appointment)*slots);// allot memory for slots
     for(int i=0;i<slots;i++){
         d->schedule[i].slotNumber = i+1;
         strcpy(d->schedule[i].patientName,"");
     }
     docCount++;
 }
-void listDoctors(){
+void listDoctors(){ // display doctors
     printf("\n------Doctor Directory------\n");
     for(int i=0;i<docCount;i++){
         printf("%d) %s [%s] Slots:%d\n",
@@ -44,14 +44,14 @@ void listDoctors(){
                 docList[i].field, docList[i].capacity);
     }
 }
-
-Doctor* findDoctor(int id) {
+// find doctor by id
+Doctor* findDoctor(int id) { 
     for(int i=0;i<docCount;i++)
         if(docList[i].id==id) 
             return &docList[i];
     return NULL;
 }
-
+//book appointment
 void bookSlot(){
     listDoctors();
     int did,slot;
@@ -66,6 +66,7 @@ void bookSlot(){
         printf("Invalid doctor. \n");
         return;
     }
+    // display slots
     printf("\n-----SLOTS-----\n");
     for(int i=0;i<d->capacity;i++){
         printf("Slot %d : %s\n",d->schedule[i].slotNumber,
@@ -84,17 +85,17 @@ void bookSlot(){
         printf("Slot already taken\n");
         return;
     }
-
-    printf("Enter the patient's name: ");
+    
+    printf("Enter the patient's name: ");//input patient naeme
     fgets(pname,64,stdin);
     pname[strcspn(pname,"\n")]=0;
 
-    strcpy(d->schedule[slot-1].patientName,pname);
+    strcpy(d->schedule[slot-1].patientName,pname);//save booking
     d->bookedCount++;
 
     printf("Appointment booked:\n");
 }
-void cancelSlot(){
+void cancelSlot(){ // cancel appointment
     listDoctors();
     int did,slot;
 
@@ -121,91 +122,91 @@ void cancelSlot(){
         return;
     }
 
-    strcpy(d->schedule[slot-1].patientName,"");
+    strcpy(d->schedule[slot-1].patientName,"");//clear patient name
     d->bookedCount--;
     
     printf("Appointmentt removed.\n");
 }
-
+//walkin patients using priority queue
 typedef struct{
 int id;
 char name[64];
 char symptoms[120];
-int priority;
+int priority; 
 time_t arrivalTime;
 }Walkin;
 
 Walkin *pq = NULL;
 int pqSize = 0;
 
-void swapWalkin(Walkin *a,Walkin *b)
+void swapWalkin(Walkin *a,Walkin *b) // swap 2 walkin records
 {
     Walkin temp = *a;
     *a=*b;
     *b=temp;
 }
 
-void pushWalkin(const char *name,const char *sym,int pr)
+void pushWalkin(const char *name,const char *sym,int pr) // enter patient in priority queue
 {
     pq = realloc(pq,sizeof(Walkin)*(pqSize+1));
-Walkin *w =&pq[pqSize];
-w->id=rand()%900 + 100;
-strcpy(w->name,name);
-strcpy(w->symptoms,sym);
-w->priority =pr;
-w->arrivalTime = time(NULL);
+    Walkin *w =&pq[pqSize];
+    w->id=rand()%900 + 100;
+    strcpy(w->name,name);
+    strcpy(w->symptoms,sym);
+    w->priority =pr;
+    w->arrivalTime = time(NULL);
 
 //simple min-heap bubble-up
-int idx =pqSize;
-while(idx>0)
-{
-int parent =(idx-1)/2;
-if(pq[parent].priority<=pq[idx].priority)
-    break;
-swapWalkin(&pq[parent],&pq[idx]);
-idx = parent;
-}
-
-pqSize++;
-printf("Walk-ins registered.\n");
-}
-
-void serveWalkin()
-{
-    if(pqSize==0){
-printf("No Walk-ins present.\n");
-return;
+    int idx =pqSize;
+    while(idx>0)
+    {
+        int parent =(idx-1)/2;
+        if(pq[parent].priority<=pq[idx].priority)
+            break;
+        swapWalkin(&pq[parent],&pq[idx]);
+        idx = parent;
     }
 
-Walkin top=pq[0];
-
-pq[0] = pq[pqSize-1];
-pqSize--;
-pq = realloc(pq,sizeof(Walkin)*pqSize);
-
-int idx=0;
-while(1)
-{
-int left=2*idx+1,right=2*idx+2;
-int smallest =idx;
-
-if(left<pqSize && pq[left].priority<pq[smallest].priority)
-    smallest=left;
-if(right<pqSize && pq[right].priority<pq[smallest].priority)
-    smallest=right;
-if(smallest==idx)
-    break;
-
-swapWalkin(&pq[idx],&pq[smallest]);
-idx=smallest;
+    pqSize++;
+    printf("Walk-ins registered.\n");
 }
 
-time_t now =time(NULL);
-double wait = difftime(now,top.arrivalTime);
+void serveWalkin() //serve the highest priority walkins
+{
+    if(pqSize==0){
+        printf("No Walk-ins present.\n");
+        return;
+    }
 
-printf("\n--NOW SERVING WALK-IN--\n");
-printf("NAme:%s|ID:%d\n",top.name,top.id);
-printf("Wait Time : %0f sec\n",wait);
+    Walkin top=pq[0]; // patient with highest priority
+
+    pq[0] = pq[pqSize-1];
+    pqSize--;
+    pq = realloc(pq,sizeof(Walkin)*pqSize);
+
+    int idx=0;
+    while(1)
+    {
+        int left=2*idx+1,right=2*idx+2;
+        int smallest =idx;
+
+        if(left<pqSize && pq[left].priority<pq[smallest].priority)
+            smallest=left;
+        if(right<pqSize && pq[right].priority<pq[smallest].priority)
+            smallest=right;
+        if(smallest==idx)
+            break;
+
+        swapWalkin(&pq[idx],&pq[smallest]);
+        idx=smallest;
+    }
+//calculate waiting time
+    time_t now =time(NULL);
+    double wait = difftime(now,top.arrivalTime);
+
+    printf("\n--NOW SERVING WALK-IN--\n");
+    printf("NAme:%s|ID:%d\n",top.name,top.id);
+    printf("Wait Time : %0f sec\n",wait);
 }
 int totalBooked =0;
 int totalCancelled=0;
@@ -217,70 +218,64 @@ void recordCancelled(){totalCancelled++;}
 
 void printReport(){
     printf("\n==========DAILY REPORT==========\n");
-for(int i=0;i<docCount;i++)
-{
-Doctor*d=&docList[i];
-printf("\nDoctor;%s\n",d->name);
-printf("Total slots: %dd\n", d->capacity);
-printf("Book: %d | Free: %d\n", d->bookedCount, d->capacity - d->bookedCount);
-}
-printf("\n----Walked-in Queue----\n");
-if(pqSize==0)
-    printf("Pending Walk-ins: 0\n");
-else
-    printf("Pending Walk-ins: %d\n",pqSize);
+    for(int i=0;i<docCount;i++)
+    {
+        Doctor*d=&docList[i];
+        printf("\nDoctor;%s\n",d->name);
+        printf("Total slots: %dd\n", d->capacity);
+        printf("Book: %d | Free: %d\n", d->bookedCount, d->capacity - d->bookedCount);
+    }
+    printf("\n----Walked-in Queue----\n");
+    if(pqSize==0)
+        printf("Pending Walk-ins: 0\n");
+    else
+        printf("Pending Walk-ins: %d\n",pqSize);
 
-printf("\nAppointments Books:%d\n",totalBooked);
-printf("Appointments Cancelled%d\n",totalCancelled);
+    printf("\nAppointments Books:%d\n",totalBooked);
+    printf("Appointments Cancelled%d\n",totalCancelled);
 }
 int main()
 {
     srand(time(NULL));
 
-addDoctor(1, "Dr.Kunal","Heart Specialist",5);
-addDoctor(2, "Dr.Dhruv","Skin clinic",4);
-addDoctor(3,"Dr.Abhinav","General Medicine",6);
-int op;
-do{
-printf("\n======CLINIC CONTROL SYSYTEM=======\n");
+    addDoctor(1, "Dr.Kunal","Heart Specialist",5);
+    addDoctor(2, "Dr.Dhruv","Skin clinic",4);
+    addDoctor(3,"Dr.Abhinav","General Medicine",6);
+    int op;
+    do{
+        printf("\n======CLINIC CONTROL SYSYTEM=======\n");
 
- printf("1.Book Appointment\n");
- printf("2.Cancelled Appointment\n");
- printf("3.Register walk-in\n");
- printf("4.Server Walk-in\n");
- printf("5.view Doctors\n");
- printf("6.Daily report\n");
- printf("0.Exit\n");
- printf("Choose\n");
+        printf("1.Book Appointment\n");
+        printf("2.Cancelled Appointment\n");
+        printf("3.Register walk-in\n");
+        printf("4.Server Walk-in\n");
+        printf("5.view Doctors\n");
+        printf("6.Daily report\n");
+        printf("0.Exit\n");
+        printf("Choose\n");
 
-scanf("%d",&op);
-getchar();
+        scanf("%d",&op);
+        getchar();
 
-if(op==1) {bookSlot();recordBooked();}
-else if(op==2){cancelSlot();recordCancelled();}
-else if(op==3){
-char n[64],s[120];
-int p;
-printf("Name:");
-fgets(n,64,stdin);n[strcspn(n,"\n")]=0;
-printf("Symptoms:");
-fgets(s,120,stdin);s[strcspn(s,"\n")]=0;
-printf("priorty(1=High,2=Medium,3=Low):");
-printf("priority (1=High,2=Medium,3=Low): ");
-scanf("%d",&p); getchar();
-pushWalkin(n,s,p);
+        if(op==1) {bookSlot();recordBooked();}
+        else if(op==2){cancelSlot();recordCancelled();}
+        else if(op==3){
+            char n[64],s[120];
+            int p;
+            printf("Name:");
+            fgets(n,64,stdin);n[strcspn(n,"\n")]=0;
+            printf("Symptoms:");
+            fgets(s,120,stdin);s[strcspn(s,"\n")]=0;
+            printf("priorty(1=High,2=Medium,3=Low):");
+            printf("priority (1=High,2=Medium,3=Low): ");
+            scanf("%d",&p); getchar();
+            pushWalkin(n,s,p);
+        }
+        else if(op==4) serveWalkin();
+        else if(op==5) listDoctors();
+        else if(op==6) printReport();
+    }
+    while(op!=0);
+    
+    return 0;
 }
-else if(op==4) serveWalkin();
-else if(op==5) listDoctors();
-else if(op==6) printReport();
-}
-while(op!=0);
-return 0;
-}
-
-
-
-
-
-
-
